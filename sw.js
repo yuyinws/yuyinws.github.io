4cab6ee0fc1664d3392be44c5689071519737bf7
lyc <lyc@pheicloud.com>
2021-03-07 19:41:57 +0800

    Site updated: 2021-03-07 19:41:57

--- sw.js
+++ sw.js
@@ -10,7 +10,7 @@
 
 'use strict';
 
-var precacheConfig = [["/sw-register.js","35f3f4b333c0700290541dcdd5d9cdf5"]];
+var precacheConfig = [];
 var cacheName = 'sw-precache-v3--' + (self.registration ? self.registration.scope : '');
 var firstRegister = 1; // 默认1是首次安装SW， 0是SW更新
 
